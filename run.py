7e6436b16ec3ef1c0003c77b23eeda5182a2a560
Mauricio Chavez <34659996+mauriciochavezpe@users.noreply.github.com>
2025-05-20 21:12:02 -0500

    update migration vercel

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Esto asegura que las tablas base existan si no usas migraciones.
         # cargarBatch()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
