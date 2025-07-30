fb9e3eb4d2b744d0d55c6d3c37c5dadea2018e0e
Mauricio Chavez <34659996+mauriciochavezpe@users.noreply.github.com>
2025-07-30 17:20:12 -0500

    agregamos manejo de seguridad jwt

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,21 @@
 from flask import Flask
 from app.config.config import Config
-from app.extension import db, migrate, jwt
+from app.extension import db, migrate, bcrypt
 from app.routes import register_blueprints
-# from app.swagger import api
-# from flasgger import Swagger
-# migrate =Migrate()
- # invocamos al servicio batch
-# from app.utils.Storage.config import authenticate_service_account
- 
- 
-def create_app():
-        app = Flask(__name__) 
-        app.config.from_object(Config)
-        # Inicializar extensiones
-        db.init_app(app)
-        migrate.init_app(app, db)  # Integrar Flask-Migrate con la app y SQLAlchemy
-        jwt.init_app(app)  # Integrar Flask-Migrate con la app y SQLAlchemy
-        register_blueprints(app)
-        # Swagger
-        # api.init_app(app)
-        # from app.models import   # ajusta según tus modelos
-        from app.models.entities import User,Customer,MasterData, Invoice, InvoiceDetails, Product, Payments, Serie
-        # drive_service = authenticate_service_account()
-        # print("Autenticación exitosa, {drive_service}")
-        
-        return app
 
- 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    # Inicializar extensiones
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    # jwt.init_app(app)
+    
+    register_blueprints(app)
+    
+    # Importar modelos para que Alembic los detecte
+    from app.models.entities import User, Customer, MasterData, Invoice, InvoiceDetails, Product, Payments, Serie
+
+    return app
--- app/models/entities/User.py
+++ app/models/entities/User.py
@@ -1,30 +1,33 @@
-from app import db
-
+from app.extension import db, bcrypt
 from .Auditoria import Auditoria
 from app.models.enums.document_types import DocumentType
+
 class User(Auditoria):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     is_active = db.Column(db.Boolean, default=True)
-    is_admin = db.Column(db.Boolean, default=False) #true si es admin, false si es cliente
+    is_admin = db.Column(db.Boolean, default=False)
     id_status = db.Column(db.Integer, nullable=False)
     document_number = db.Column(db.String(20), unique=True, nullable=False)
 
-    
-    # POR DEFECTO TRAERA 1 - DNI
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
     @property
     def document_type_description(self):
         doc_type = DocumentType.get_by_code(self.document_type)
         return doc_type.description if doc_type else None
-    
+
     def to_dict(self):
         return {
             'id': self.id,
             'username': self.username,
             'email': self.email,
-            'password': self.password,
             'is_active': self.is_active,
             'is_admin': self.is_admin,
             'id_status': self.id_status,
